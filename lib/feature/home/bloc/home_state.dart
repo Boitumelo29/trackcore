536c1ef1009c49895df27de5f4bb63257be0574d
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-06-03 12:22:22 +0200

    WIP: creating home_bloc.dart

--- lib/feature/home/bloc/home_state.dart
+++ lib/feature/home/bloc/home_state.dart
@@ -3,6 +3,9 @@ part of 'home_bloc.dart';
 @freezed
 class HomeState with _$HomeState {
   const factory HomeState({
+    @Default([]) List<TripEntity> trips,
+    @Default(false) bool isLoading,
+    @Default(None()) Option<Either<Failure, Unit>> tripFailureOrSuccess,
     @Default("unknown") userName,
     @Default(None()) Option<Either<Failure, Unit>> logoutFailureFailureOrUnit,
   }) = _HomeState;
