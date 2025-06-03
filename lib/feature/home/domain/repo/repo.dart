1eed4014f197070e0d2114668ec8363f06c3770a
tumi.jane <77927688+Boitumelo29@users.noreply.github.com>
2025-06-03 18:31:09 +0200

    WIP: updating repo

--- lib/feature/home/domain/repo/repo.dart
+++ lib/feature/home/domain/repo/repo.dart
@@ -4,4 +4,7 @@ import 'package:trackcore/feature/home/domain/entity/trip_entity.dart';
 
 abstract class TripRepository {
   Future<Either<Failure, List<TripEntity>>> getTrips();
+
+  Future<Either<Failure, TripNotesResponseModel>> setTripNotes(
+      {required String ID, required String note});
 }
